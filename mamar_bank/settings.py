53943c10b71b454aebf22b411112164c869dcc90
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-11 02:07:09 +0600

    csrf trusted origins

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-7isl.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-a5ip.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
